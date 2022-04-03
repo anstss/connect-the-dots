9e81d51a09d5a2b377e70c41756cc6495a9ec631
anstss <anastasiya31017@gmail.com>
2022-04-03 17:55:12 +0300

    refactor: move methods from scene to Dots class

--- connect-the-dots/scripts/game-scene.js
+++ connect-the-dots/scripts/game-scene.js
@@ -5,76 +5,12 @@ class GameScene extends Phaser.Scene {
 
   create() {
     this.dots = new Dots(this);
-    this.markedDots = [];
-    this.input.on('gameobjectdown', (pointer, gameObject) => this.startDrawingLine(gameObject));
-    this.input.on('pointerup', this.stopDrawingLine, this);
-    this.input.on('gameobjectover', (pointer, gameObject) => this.markDot(gameObject));
-  }
-
-  startDrawingLine(dot) {
-    this.markedDots.push(dot);
-    this.activeDot = dot;
-    this.activeDot.isActive = true;
-    this.activeDot.isMarked = true;
-    this.activeDot.connector = new Connector(this, this.activeDot.x, this.activeDot.y, game.input.activePointer.x, game.input.activePointer.y, this.activeDot.fillColor);
-  }
-
-  stopDrawingLine() {
-    if (this.activeDot) {
-      this.activeDot.isActive = false;
-      this.activeDot.connector.x2 = this.activeDot.x;
-      this.activeDot.connector.y2 = this.activeDot.y;
-      this.activeDot.connector.redraw();
-    }
-  }
-
-  createDotConnection(newDot) {
-    const connectionAllowed = this.connectionAllowed(this.activeDot, newDot);
-    if (this.activeDot && connectionAllowed) {
-      this.activeDot.isActive = false;
-      this.activeDot.connector.x2 = newDot.x;
-      this.activeDot.connector.y2 = newDot.y;
-      this.activeDot.connector.redraw();
-      this.startDrawingLine(newDot);
-    }
-  }
-
-  updateLine() {
-    if (game.input.activePointer.isDown && this.activeDot && this.activeDot.isActive) {
-      this.activeDot.connector.x2 = game.input.activePointer.x;
-      this.activeDot.connector.y2 = game.input.activePointer.y;
-      this.activeDot.connector.redraw();
-    }
-  }
-
-  markDot(dot) {
-    if (game.input.activePointer.isDown && dot.isActive && this.markedDots.length > 1) {
-      this.unmarkDot(dot);
-    } else if (game.input.activePointer.isDown && !dot.isMarked && this.activeDot) {
-      this.createDotConnection(dot);
-    }
-  }
-
-  unmarkDot(dot) {
-    dot.isActive = false;
-    dot.isMarked = false;
-    dot.connector.graphics.destroy();
-    this.markedDots.pop();
-    this.activeDot = this.markedDots[this.markedDots.length - 1];
-    this.activeDot.isActive = true;
-    this.updateLine();
-  }
-
-  connectionAllowed(prevDot, currentDot) {
-    const allowedColor = prevDot.fillColor === currentDot.fillColor;
-    const allowedDirections = prevDot.x === currentDot.x || prevDot.y === currentDot.y;
-    const allowedAxisOffset = prevDot.margin + config.dotRadius * 2;
-    const allowedX = currentDot.x === prevDot.x + allowedAxisOffset || currentDot.x === prevDot.x - allowedAxisOffset;
-    const allowedY = currentDot.y === prevDot.y + allowedAxisOffset || currentDot.y === prevDot.y - allowedAxisOffset;
-    return (allowedColor && allowedX || allowedColor && allowedY) && allowedDirections;
+    this.input.on('gameobjectdown', (pointer, gameObject) => this.dots.startDrawingLine(gameObject));
+    this.input.on('pointerup', this.dots.stopDrawingLine, this.dots);
+    this.input.on('gameobjectover', (pointer, gameObject) => this.dots.markDot(gameObject));
   }
 
   update() {
-    this.updateLine();
+    this.dots.updateLine();
   }
 }
--- connect-the-dots/scripts/prefabs/dots.js
+++ connect-the-dots/scripts/prefabs/dots.js
@@ -7,6 +7,7 @@ class Dots extends Phaser.GameObjects.Group {
 
   init() {
     this.fillGroup();
+    this.markedDots = [];
   }
 
   fillGroup() {
@@ -15,5 +16,68 @@ class Dots extends Phaser.GameObjects.Group {
         this.add(Dot.generateDot(this.scene, r, c));
       }
     }
+  }
+
+  startDrawingLine(dot) {
+    this.markedDots.push(dot);
+    this.activeDot = dot;
+    this.activeDot.isActive = true;
+    this.activeDot.isMarked = true;
+    this.activeDot.connector = new Connector(this.scene, this.activeDot.x, this.activeDot.y, game.input.activePointer.x, game.input.activePointer.y, this.activeDot.fillColor);
+  }
+
+  stopDrawingLine() {
+    if (this.activeDot) {
+      this.activeDot.isActive = false;
+      this.activeDot.connector.x2 = this.activeDot.x;
+      this.activeDot.connector.y2 = this.activeDot.y;
+      this.activeDot.connector.redraw();
+    }
+  }
+
+  createDotConnection(newDot) {
+    const connectionAllowed = this.connectionAllowed(this.activeDot, newDot);
+    if (this.activeDot && connectionAllowed) {
+      this.activeDot.isActive = false;
+      this.activeDot.connector.x2 = newDot.x;
+      this.activeDot.connector.y2 = newDot.y;
+      this.activeDot.connector.redraw();
+      this.startDrawingLine(newDot);
+    }
+  }
+
+  updateLine() {
+    if (game.input.activePointer.isDown && this.activeDot && this.activeDot.isActive) {
+      this.activeDot.connector.x2 = game.input.activePointer.x;
+      this.activeDot.connector.y2 = game.input.activePointer.y;
+      this.activeDot.connector.redraw();
+    }
+  }
+
+  markDot(dot) {
+    if (game.input.activePointer.isDown && dot.isActive && this.markedDots.length > 1) {
+      this.unmarkDot(dot);
+    } else if (game.input.activePointer.isDown && !dot.isMarked && this.activeDot) {
+      this.createDotConnection(dot);
+    }
+  }
+
+  unmarkDot(dot) {
+    dot.isActive = false;
+    dot.isMarked = false;
+    dot.connector.graphics.destroy();
+    this.markedDots.pop();
+    this.activeDot = this.markedDots[this.markedDots.length - 1];
+    this.activeDot.isActive = true;
+    this.updateLine();
+  }
+
+  connectionAllowed(prevDot, currentDot) {
+    const allowedColor = prevDot.fillColor === currentDot.fillColor;
+    const allowedDirections = prevDot.x === currentDot.x || prevDot.y === currentDot.y;
+    const allowedAxisOffset = prevDot.margin + config.dotRadius * 2;
+    const allowedX = currentDot.x === prevDot.x + allowedAxisOffset || currentDot.x === prevDot.x - allowedAxisOffset;
+    const allowedY = currentDot.y === prevDot.y + allowedAxisOffset || currentDot.y === prevDot.y - allowedAxisOffset;
+    return (allowedColor && allowedX || allowedColor && allowedY) && allowedDirections;
   }
 }
